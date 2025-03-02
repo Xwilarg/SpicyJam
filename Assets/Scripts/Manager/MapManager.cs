57cbeae1531bfba8c4bd43e4942ee9104d7639b2
Xwilarg <xwilarg@protonmail.com>
2025-03-02 15:21:39 +0100

    Add NPC movement between nodes

--- Assets/Scripts/Manager/MapManager.cs
+++ Assets/Scripts/Manager/MapManager.cs
@@ -6,9 +6,15 @@ namespace SpicyJam.Manager
     {
         public static MapManager Instance { private set; get; }
 
+        [SerializeField]
+        private Transform[] _nodes;
+
         private void Awake()
         {
             Instance = this;
         }
+
+        public Transform GetRandomNode()
+            => _nodes[Random.Range(0, _nodes.Length)];
     }
 }
--- Assets/Scripts/NPC/NpcController.cs
+++ Assets/Scripts/NPC/NpcController.cs
@@ -1,8 +1,32 @@
+using SpicyJam.Manager;
 using UnityEngine;
 
 namespace SpicyJam.NPC
 {
     public class NpcController : MonoBehaviour
     {
+        [SerializeField]
+        private float _speed;
+
+        private Transform _target;
+        private Rigidbody2D _rb;
+
+        private void Start()
+        {
+            _target = MapManager.Instance.GetRandomNode();
+            _rb = GetComponent<Rigidbody2D>();
+        }
+
+        private void Update()
+        {
+            if (Vector2.Distance(_target.position, transform.position) < .1f)
+            {
+                _target = MapManager.Instance.GetRandomNode();
+            }
+            else
+            {
+                _rb.linearVelocity = (_target.position - transform.position).normalized * _speed;
+            }
+        }
     }
 }
