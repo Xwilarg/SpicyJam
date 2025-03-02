1f4eb1e77847745d8e78ca4bc2cc08ea1d4a9e6c
Xwilarg <xwilarg@protonmail.com>
2025-03-02 20:11:01 +0100

    Add priest description

--- Assets/Scripts/NPC/NpcController.cs
+++ Assets/Scripts/NPC/NpcController.cs
@@ -2,6 +2,7 @@ using SpicyJam.Interaction;
 using SpicyJam.Manager;
 using SpicyJam.Player;
 using System.Collections.Generic;
+using System.Text;
 using UnityEngine;
 
 namespace SpicyJam.NPC
@@ -22,10 +23,12 @@ namespace SpicyJam.NPC
         public bool IsVampire { set; get; }
         public bool IsPriest { set; get; }
 
-        private string[] _eyesColors = new[] { "blue", "yellow", "brown" };
-        private string[] _hairColors = new[] { "blond", "light brown", "dark brown", "red" };
-        private string[] _gender = new[] { "female", "male" };
-        private string[] _age = new[] { "young", "old" };
+        private readonly string[] _eyesColors = new[] { "blue", "yellow", "brown" };
+        private readonly string[] _hairColors = new[] { "blond", "light brown", "dark brown", "red" };
+        private readonly string[] _gender = new[] { "female", "male" };
+        private readonly string[] _age = new[] { "young", "old" };
+
+        private readonly string[] _pronouns = new[] { "her", "his" };
         private readonly Dictionary<string, string> _attrs = new();
 
         public void Interact(PlayerController pc)
@@ -45,7 +48,9 @@ namespace SpicyJam.NPC
             _baseColor = _sr.color;
             _attrs.Add("eyesColor", _eyesColors[Random.Range(0, _eyesColors.Length)]);
             _attrs.Add("hairColor", _hairColors[Random.Range(0, _hairColors.Length)]);
-            _attrs.Add("gender", _gender[Random.Range(0, _gender.Length)]);
+            var gender = Random.Range(0, _gender.Length);
+            _attrs.Add("gender", _gender[gender]);
+            _attrs.Add("pronouns", _pronouns[gender]);
             _attrs.Add("age", _age[Random.Range(0, _age.Length)]);
         }
 
@@ -56,7 +61,13 @@ namespace SpicyJam.NPC
 
         public string GetDescription()
         {
-            return $"This person seem to be a {_attrs["age"]} {_attrs["gender"]} with {_attrs["eyesColor"]} eyes and {_attrs["hairColor"]} hair";
+            StringBuilder str = new();
+            str.AppendLine($"This person seem to be a {_attrs["age"]} {_attrs["gender"]} with {_attrs["eyesColor"]} eyes and {_attrs["hairColor"]} hair");
+            if (IsPriest)
+            {
+                str.AppendLine($"From {_attrs["gender"]} outfits, you can guess this person is a priest");
+            }
+            return str.ToString();
         }
 
         private void Update()
