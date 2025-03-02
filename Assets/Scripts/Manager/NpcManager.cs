0de2136298eba10ff0065bac2e5433679676922e
Xwilarg <xwilarg@protonmail.com>
2025-03-02 15:03:56 +0100

    Add NPC spawner

--- Assets/Scripts/Manager/NpcManager.cs
+++ Assets/Scripts/Manager/NpcManager.cs
@@ -5,6 +5,26 @@ namespace SpicyJam.Manager
     public class NpcManager : MonoBehaviour
     {
         [SerializeField]
+        private float _spawnAreaRadius;
+
+        [SerializeField]
         private GameObject _npcPrefab;
+
+        [SerializeField]
+        private int _spawnCount;
+
+        private void Awake()
+        {
+            for (int i = 0; i < _spawnCount; i++)
+            {
+                Instantiate(_npcPrefab, Random.insideUnitCircle * _spawnAreaRadius, Quaternion.identity);
+            }
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawWireSphere(Vector3.zero, _spawnAreaRadius);
+        }
     }
 }
